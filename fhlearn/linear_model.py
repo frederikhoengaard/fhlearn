f7de68be16c6038ced56c48858d0a268b9e4e466
Frederik Høngaard <mail@frederikhoengaard.com>
2021-02-28 00:32:04 +0100

    Update linear_model.py

--- fhlearn/linear_model.py
+++ fhlearn/linear_model.py
@@ -1,19 +1,19 @@
 import numpy as np
 
 
-class LinearRegressionModel:
+class LinearRegressionModel: # HML p 106
     def __init__(self):
         self.theta = None
         self.X = None
         self.y = None
 
-    def fit(self,features,targets):
+    def fit(self, features, targets):
         self.X = np.copy(features)
         self.y = np.copy(targets)
         self.X = np.c_[np.ones((len(self.X), 1)), self.X] # add x0 = 1 to each instance
         self.theta = np.linalg.inv(self.X.T.dot(self.X)).dot(self.X.T).dot(self.y)
        
-    def predict(self,features):
+    def predict(self, features):
         X_new = np.c_[np.ones((len(features), 1)), features] # add x0 = 1 to each instance
         return X_new.dot(self.theta)
 

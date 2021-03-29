3fe774e4ec9c300a7396286e53bbd5f90b79511f
Frederik Høngaard <mail@frederikhoengaard.com>
2021-03-30 00:04:34 +0200

    Update metrics.py

--- fhlearn/metrics.py
+++ fhlearn/metrics.py
@@ -6,18 +6,13 @@ def accuracy_score(
         predicted_labels,
         normalize: bool = True,
     ) -> float:   
-    
     n_samples_true, n_samples_predicted = len(true_labels), len(predicted_labels)
-
     if n_samples_true != n_samples_predicted:
         raise ValueError()
-
     n_correct_predictions = 0
-
     for i in range(len(true_labels)):
         if  true_labels[i] == predicted_labels[i]:
             n_correct_predictions += 1
-    
     if normalize:
         return n_correct_predictions / n_samples_true
     else:
@@ -28,13 +23,11 @@ def _get_class_weights(labels) -> dict:
     classes = set(labels)
     class_weights = {}
     class_counts = {}
-
     for value in labels:
         if value not in class_counts:
             class_counts[value] = 1
         else:
             class_counts[value] += 1
-    
     for class_ in classes:
         class_weights[class_] = class_counts[class_] / len(labels)
     return class_weights
@@ -44,11 +37,9 @@ def _compute_positives_and_negatives(
         true_labels,
         predicted_labels
     ) -> dict:
-
     classes = set(true_labels)
     records = {}
     weights = _get_class_weights(true_labels)
-
     for class_ in classes:
         true_positives, false_positives, true_negatives, false_negatives = 0, 0, 0, 0
         for i in range(len(true_labels)):
@@ -76,15 +67,11 @@ def confusion_matrix(
     Currently requires that n classes be encoded as 0,n-1. Otherwise it will break.
     Consider using preprocessing.LabelEncoder
     """
-
     n_samples_true, n_samples_predicted = len(true_labels), len(predicted_labels)
-
     if n_samples_true != n_samples_predicted:
         raise ValueError()
-
     n_classes = len(set(true_labels))
     matrix = np.zeros((n_classes,n_classes))
-    
     for i in range(len(true_labels)):
         true_label = true_labels[i]
         predicted_label = predicted_labels[i]
@@ -98,16 +85,12 @@ def precision_score(
         average=None,
         zero_division=0
     ):
-
     n_samples_true, n_samples_predicted = len(true_labels), len(predicted_labels)
-
     if n_samples_true != n_samples_predicted:
         raise ValueError()
-
     records = _compute_positives_and_negatives(true_labels,predicted_labels)
     classes = records.keys()
     precision_dict = {}
-
     for class_ in classes:
         tp, fp = records[class_]['tp'], records[class_]['fp']
         sum_positives = tp + fp
@@ -115,7 +98,6 @@ def precision_score(
             precision_dict[class_] = zero_division
         else:
             precision_dict[class_] = tp / sum_positives
-    
     if not average:
         return precision_dict # class-specific precision
     else:
@@ -142,16 +124,12 @@ def recall_score(
         average=None,
         zero_division=0
     ):
-
     n_samples_true, n_samples_predicted = len(true_labels), len(predicted_labels)
-
     if n_samples_true != n_samples_predicted:
         raise ValueError()
-
     records = _compute_positives_and_negatives(true_labels,predicted_labels)
     classes = records.keys()
     recall_dict = {}
-
     for class_ in classes:
         tp, fn = records[class_]['tp'], records[class_]['fn']
         potential_positives = tp + fn
@@ -159,7 +137,6 @@ def recall_score(
             recall_dict[class_] = zero_division
         else:
             recall_dict[class_] = tp / potential_positives
-
     if not average:
         return recall_dict
     else:
@@ -186,24 +163,19 @@ def f1_score(
         average=None,
         zero_division=0
     ):
-
     n_samples_true, n_samples_predicted = len(true_labels), len(predicted_labels)
-
     if n_samples_true != n_samples_predicted:
         raise ValueError()
-    
     precision = precision_score(true_labels,predicted_labels,zero_division=zero_division)
     recall = recall_score(true_labels,predicted_labels,zero_division=zero_division)
     weights = _get_class_weights(true_labels)
     f1_dict = {}  
-    
     for class_ in precision.keys():
         sum_recall_precision = precision[class_] + recall[class_]
         if sum_recall_precision == 0:
             f1_dict[class_] = zero_division
         else:
             f1_dict[class_] = 2 * (precision[class_] * recall[class_]) / sum_recall_precision
-    
     if not average:
         return f1_dict
     else:
@@ -227,21 +199,18 @@ def f1_score(
 def sum_squared_errors(true_targets: np.array, predicted_targets: np.array):
     if not np.shape(true_targets) == np.shape(predicted_targets):
         raise ValueError('Input arrays not of equal dimensions')
-    
     return np.sum((true_targets - np.mean(true_targets)) ** 2)
 
 
 def sum_squared_residuals(true_targets: np.array, predicted_targets: np.array):
     if not np.shape(true_targets) == np.shape(predicted_targets):
         raise ValueError('Input arrays not of equal dimensions')
-    
     return np.sum((true_targets - predicted_targets) ** 2)
 
 
 def mean_squared_error(true_targets: np.array, predicted_targets: np.array, squared=True):
     if not np.shape(true_targets) == np.shape(predicted_targets):
         raise ValueError('Input arrays not of equal dimensions')
-
     if squared:
         return np.mean((true_targets - predicted_targets) ** 2) 
     else:
@@ -251,5 +220,4 @@ def mean_squared_error(true_targets: np.array, predicted_targets: np.array, squa
 def r2_score(true_targets: np.array, predicted_targets: np.array) -> float:
     if not np.shape(true_targets) == np.shape(predicted_targets):
         raise ValueError('Input arrays not of equal dimensions')
-    
     return 1 - (sum_squared_residuals(true_targets,predicted_targets) / sum_squared_errors(true_targets,predicted_targets))
